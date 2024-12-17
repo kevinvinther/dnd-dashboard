9e3adb911716948ebeded89f2326b247986231a8
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2024-12-17 19:28:59 +0100

    Add favicons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://kevinvinther.github.io',
-  base: 'dnd-dashboard',
+  site: "https://kevinvinther.github.io",
+  base: "dnd-dashboard",
 });
